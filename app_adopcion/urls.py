50883c35438bc74be97e7962ce48ac7dbbefe89d
Victor Davila <victordavila04@gmail.com>
2023-02-27 11:29:17 -0500

    Se hizo la herencia de adopcion y mascota

--- app_adopcion/urls.py
+++ app_adopcion/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from app_adopcion import views
 
 urlpatterns = [
-    path('',views.base),
+    path('',views.base, name='Origin'),
     path('adopcion', views.adopcion, name="Adopcion"),
 ]
--- app_mascota/urls.py
+++ app_mascota/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from app_mascota import views
 
 urlpatterns = [
-    path('',views.base),
+    path('',views.base, name = "Origin"),
     path('mascota', views.mascota, name="Mascota"),
 ]
