cc4528247523e58d3f6d971a221a90851c30321e
Victor Davila <victordavila04@gmail.com>
2023-02-28 18:03:26 -0500

    Se agrega el parametro para editar y eliminar en el formulario

--- app_mascota/urls.py
+++ app_mascota/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('mascota/', views.mascota, name="Mascota"),
     path('mascotaform/', views.mascota_view, name="Mascotaform"),
     path('mascotalist/', views.mascota_list, name="Mascotalist"),
+    path('mascotaedit/<id_mascota>', views.mascota_edit, name="Mascotaedit"),
+    path('mascotadelete/<id_mascota>', views.mascota_delete, name="Mascotadelete"),
 ]
--- app_mascota/views.py
+++ app_mascota/views.py
@@ -33,7 +33,28 @@ def mascota_view (request):
 ### Aca voy hacer una lista en mascota 
 
 def mascota_list (request):
-    mascota = Mascota.objects.all()
+    mascota = Mascota.objects.all().order_by('id')
     contexto = {'mascota':mascota}
     return render(request, 'mascota/mascota_list.html', contexto)
+
+### Aca voy a definir una funcion para eliminar y otra para editar, tener en cuenta que vamos a recibir un id de mascota
+
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('Mascotalist')
+    return render(request, 'mascota/mascota_form.html', {'form':form})
+
+
+def mascota_delete(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'POST':
+        mascota.delete()
+        return redirect('Mascotalist')
+    return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
             
