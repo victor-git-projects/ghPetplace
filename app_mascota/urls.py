27ca1be693588f478eac9fc0912d83a5b7d76381
Victor Davila <victordavila04@gmail.com>
2023-02-28 21:21:52 -0500

    Se paso todas las vistas de funcion a vistas de clase

--- app_mascota/urls.py
+++ app_mascota/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 from app_mascota import views
+from app_mascota.views import MascotaList, MascotaCreate, MascotaUpdate, MascotaDelete
 
 urlpatterns = [
     path('',views.base, name = "Origin"),
     path('mascota/', views.mascota, name="Mascota"),
-    path('mascotaform/', views.mascota_view, name="Mascotaform"),
-    path('mascotalist/', views.mascota_list, name="Mascotalist"),
-    path('mascotaedit/<id_mascota>', views.mascota_edit, name="Mascotaedit"),
-    path('mascotadelete/<id_mascota>', views.mascota_delete, name="Mascotadelete"),
+    
+    #Aca es mi lista pero se llama basado en funciones pero ahora abajo la estoy colocando como vista por clase.
+    #path('mascotaform/', views.mascota_view, name="Mascotaform"),
+    path('mascotaform/', MascotaCreate.as_view(), name="Mascotaform"),
+    
+    #Aca es mi lista pero se llama basado en funciones pero ahora abajo la estoy colocando como vista por clase.
+    #path('mascotalist/', views.mascota_list, name="Mascotalist"),
+    path('mascotalist/', MascotaList.as_view(), name="Mascotalist"),
+    
+    #path('mascotaedit/<id_mascota>', views.mascota_edit, name="Mascotaedit"),
+    path('mascotaedit/<pk>', MascotaUpdate.as_view(), name="Mascotaedit"),
+    
+    
+    #path('mascotadelete/<id_mascota>', views.mascota_delete, name="Mascotadelete"),
+    path('mascotadelete/<pk>', MascotaDelete.as_view(), name="Mascotadelete"),
 ]
--- app_mascota/views.py
+++ app_mascota/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from app_mascota.forms import MascotaForm
 from app_mascota.models import Mascota
 
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
 # Create your views here.
 
 #def inicio (request):
@@ -19,36 +22,36 @@ def mascota (request):
     return render(request,'mascota/index.html')
     #return HttpResponse("Esta es la vista de mascotas")
     
-def mascota_view (request):
-    if request.method == 'POST':
-        form = MascotaForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('Origin')
-    else:
-        form = MascotaForm()
-    return render(request, 'mascota/mascota_form.html', {'form': form})
+#def mascota_view (request):
+#    if request.method == 'POST':
+#        form = MascotaForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#        return redirect('Origin')
+#    else:
+#        form = MascotaForm()
+#    return render(request, 'mascota/mascota_form.html', {'form': form})
 
 
-### Aca voy hacer una lista en mascota 
-
-def mascota_list (request):
-    mascota = Mascota.objects.all().order_by('id')
-    contexto = {'mascota':mascota}
-    return render(request, 'mascota/mascota_list.html', contexto)
+### Aca voy hacer una lista en mascota pero esto es una vista por medio de funcion, abajo active la vista por medio de clase entonces esto ya no seria necesario
+#def mascota_list (request):
+#    mascota = Mascota.objects.all().order_by('id')
+#    contexto = {'mascota':mascota}
+#    return render(request, 'mascota/mascota_list.html', contexto)
 
 ### Aca voy a definir una funcion para eliminar y otra para editar, tener en cuenta que vamos a recibir un id de mascota
 
-def mascota_edit(request, id_mascota):
-    mascota = Mascota.objects.get(id=id_mascota)
-    if request.method == 'GET':
-        form = MascotaForm(instance=mascota)
-    else:
-        form = MascotaForm(request.POST, instance=mascota)
-        if form.is_valid():
-            form.save()
-        return redirect('Mascotalist')
-    return render(request, 'mascota/mascota_form.html', {'form':form})
+## lo acabo de desabilitar porque lo he puesto como clase es la UpdateView que hace el tema de editar en vez de funciones
+#def mascota_edit(request, id_mascota):
+#    mascota = Mascota.objects.get(id=id_mascota)
+#    if request.method == 'GET':
+#        form = MascotaForm(instance=mascota)
+#    else:
+#        form = MascotaForm(request.POST, instance=mascota)
+#        if form.is_valid():
+#            form.save()
+#        return redirect('Mascotalist')
+#    return render(request, 'mascota/mascota_form.html', {'form':form})
 
 
 def mascota_delete(request, id_mascota):
@@ -57,4 +60,27 @@ def mascota_delete(request, id_mascota):
         mascota.delete()
         return redirect('Mascotalist')
     return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
+
+
+## Aca voy hacer una vista basada en clase ya no en funcion, ojo debo de importar el ListView de Django
             
+class MascotaList(ListView):
+    model = Mascota
+    template_name = 'mascota/mascota_list.html'
+    
+class MascotaCreate(CreateView):
+    model = Mascota
+    form_class = MascotaForm
+    template_name = 'mascota/mascota_form.html'
+    success_url = reverse_lazy('Mascotalist')
+    
+class MascotaUpdate(UpdateView):
+    model = Mascota
+    form_class = MascotaForm
+    template_name = 'mascota/mascota_form.html'
+    success_url = reverse_lazy('Mascotalist')
+    
+class MascotaDelete(DeleteView):
+    model = Mascota
+    template_name = 'mascota/mascota_delete.html'
+    success_url = reverse_lazy('Mascotalist')
